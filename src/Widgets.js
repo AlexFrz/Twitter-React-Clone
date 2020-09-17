08eabbefa111992b16b4f8e3956399f27d3462f5
Alex Frezoul <frezoul.alex@gmail.com>
2020-09-17 14:15:45 +0200

    front-end over

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,33 @@
 import React from "react";
 import "./Widgets.css";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />{" "}
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId={"1040676957938016257"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="nytimes"
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton
+          url={"https://imaleph.netlify.app/"}
+          options={{ text: "#reactjs is awesome", via: "imaleph" }}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBSgzxQ95yVGcw8QdwIpaczX4uf23M3SjE",
+  authDomain: "twitter-clone-358c7.firebaseapp.com",
+  databaseURL: "https://twitter-clone-358c7.firebaseio.com",
+  projectId: "twitter-clone-358c7",
+  storageBucket: "twitter-clone-358c7.appspot.com",
+  messagingSenderId: "323671164503",
+  appId: "1:323671164503:web:2e4de6876d1c9f3a44c512",
+  measurementId: "G-THTN4RBTTH",
+};
